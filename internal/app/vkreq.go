f726d88db8cc2a46c6ad3ca57403e2987c216a4d
Бойко Вячеслав Сергеевич <vsbojko1@mts.ru>
2020-10-03 19:09:57 +0300

    fix request structure

--- internal/app/vkreq.go
+++ internal/app/vkreq.go
@@ -11,34 +11,47 @@ import (
 )
 
 type VKReq struct {
-	Type    ReqType `json:"type"`
-	GroupID int     `json:"group_id"`
-	Object  struct {
-		Date                  int64  `json:"date"`
-		FromID                int64  `json:"from_id"`
-		ID                    int64  `json:"id"`
-		Out                   int64  `json:"out"`
-		PeerID                int64  `json:"peer_id"`
-		Text                  string `json:"text"`
-		ConversationMessageID int64  `json:"conversation_message_id"`
-		Important             bool   `json:"important"`
-		RandomID              int64  `json:"random_id"`
-		IsHidden              bool   `json:"is_hidden"`
+	Type   ReqType `json:"type"`
+	Object struct {
+		Message struct {
+			Date                  int           `json:"date"`
+			FromID                int           `json:"from_id"`
+			ID                    int           `json:"id"`
+			Out                   int           `json:"out"`
+			PeerID                int           `json:"peer_id"`
+			Text                  string        `json:"text"`
+			ConversationMessageID int           `json:"conversation_message_id"`
+			FwdMessages           []interface{} `json:"fwd_messages"`
+			Important             bool          `json:"important"`
+			RandomID              int           `json:"random_id"`
+			Attachments           []interface{} `json:"attachments"`
+			IsHidden              bool          `json:"is_hidden"`
+		} `json:"message"`
+		ClientInfo struct {
+			ButtonActions  []string `json:"button_actions"`
+			Keyboard       bool     `json:"keyboard"`
+			InlineKeyboard bool     `json:"inline_keyboard"`
+			Carousel       bool     `json:"carousel"`
+			LangID         int      `json:"lang_id"`
+		} `json:"client_info"`
 	} `json:"object"`
+	GroupID int    `json:"group_id"`
+	EventID string `json:"event_id"`
+	Secret  string `json:"secret"`
 }
 
 func (vkr VKReq) SendResult(text string, gen *generator.Generator, c config.Config) error {
-	if vkr.Object.Text[0] != byte('/') {
+	if vkr.Object.Message.Text[0] != byte('/') {
 		return nil
 	}
 	method := "messages.send"
 	params := make(map[string]string)
-	if vkr.Object.PeerID > 2000000000 {
-		params["chat_id"] = strconv.Itoa(int(vkr.Object.PeerID - 2000000000))
+	if vkr.Object.Message.PeerID > 2000000000 {
+		params["chat_id"] = strconv.Itoa(int(vkr.Object.Message.PeerID - 2000000000))
 	} else {
-		params["reply_to"] = strconv.Itoa(int(vkr.Object.ConversationMessageID))
+		params["reply_to"] = strconv.Itoa(int(vkr.Object.Message.ConversationMessageID))
 	}
-	params["peer_id"] = strconv.Itoa(int(vkr.Object.PeerID))
+	params["peer_id"] = strconv.Itoa(int(vkr.Object.Message.PeerID))
 	params["random_id"] = strconv.Itoa(int(gen.Random(9000000, 9999999)))
 	params["message"] = text
 
