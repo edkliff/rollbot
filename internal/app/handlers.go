5219043c5981a4eea7fd927c039b38f27afafa0a
Бойко Вячеслав Сергеевич <vsbojko1@mts.ru>
2020-10-03 16:49:59 +0300

    confirmation

--- internal/app/handlers.go
+++ internal/app/handlers.go
@@ -20,7 +20,14 @@ func (rb *RollBot) VKHandle(w http.ResponseWriter, req *http.Request) {
 	}
 	fmt.Println(string(body))
 	fmt.Println(vkreq)
-
+	if vkreq.Type == Confirm {
+		b := []byte("61543fb6")
+		_, err = w.Write(b)
+		if err != nil {
+			log.Println(err)
+		}
+		return
+	}
 	err = vkreq.SendResult("Бот на обслуживании", rb.Generator, rb.Config)
 	b := []byte("ok")
 	_, err = w.Write(b)
